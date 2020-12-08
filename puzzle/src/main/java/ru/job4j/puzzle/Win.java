5746a05e64cf8f356f259336fb373ba1f3a239c2
Администратор <xatowar@gmail.com>
2020-12-08 16:16:16 +0300

    Тестовое задание

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,29 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1) {
+                if (Win.checkCol(board, i) || Win.checkRow(board, i))
+                    rsl = true;
+            }
+        }
+        return  rsl;
+    }
+    public static boolean checkRow(int[][] board, int col) {
         boolean rsl = true;
-        return rsl;
+        for (int row = 0; row < board.length; row++) {
+            if(board[row][col] == 0)
+                rsl = false;
+        }
+    return  rsl;
+    }
+    public static boolean checkCol(int[][] board, int row) {
+        boolean rsl = true;
+        for (int col = 0; col < board.length; row++) {
+            if(board[row][col] == 0)
+                rsl = false;
+        }
+        return  rsl;
     }
 }
