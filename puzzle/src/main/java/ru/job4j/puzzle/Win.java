77de042dca302c3eab8e7bd199688c41b324a2af
Администратор <xatowar@gmail.com>
2020-12-09 23:59:56 +0300

    Тестовое задание

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,27 +4,33 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1) {
-                if (Win.checkCol(board, i) || Win.checkRow(board, i))
+            if(board[i][i] == 1){
+                if(Win.cell(board, i) || Win.row(board, i))
                     rsl = true;
             }
         }
-        return  rsl;
-    }
-    public static boolean checkRow(int[][] board, int col) {
-        boolean rsl = true;
-        for (int row = 0; row < board.length; row++) {
-            if(board[row][col] == 0)
-                rsl = false;
+        return rsl;
         }
-    return  rsl;
-    }
-    public static boolean checkCol(int[][] board, int row) {
+
+        public static boolean row(int [][] board, int cell){
         boolean rsl = true;
-        for (int col = 0; col < board.length; row++) {
-            if(board[row][col] == 0)
-                rsl = false;
+            for(int row = 0; row < board.length; row++) {
+                if(board[row][cell] == 0){
+                    rsl = false;
+                    break;
+                }
+            }
+            return rsl;
         }
-        return  rsl;
+
+    public static boolean cell(int [][] board, int row){
+        boolean rsl = true;
+        for (int cell = 0; cell < board.length; cell++) {
+            if(board[row][cell] == 0){
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
     }
-}
+    }
