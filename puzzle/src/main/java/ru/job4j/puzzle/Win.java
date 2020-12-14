e5addf9ff86ff5aadea0dd2e0a478d6d902ebf8c
Администратор <xatowar@gmail.com>
2020-12-14 14:22:10 +0300

    Тестовое задание

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,8 +5,10 @@ public class Win {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
             if(board[i][i] == 1){
-                if(Win.cell(board, i) || Win.row(board, i))
+                if(Win.cell(board, i) || Win.row(board, i)) {
                     rsl = true;
+                    break;
+                }
             }
         }
         return rsl;
@@ -15,7 +17,7 @@ public class Win {
         public static boolean row(int [][] board, int cell){
         boolean rsl = true;
             for(int row = 0; row < board.length; row++) {
-                if(board[row][cell] == 0){
+                if(board[row][cell] == 0) {
                     rsl = false;
                     break;
                 }
@@ -26,7 +28,7 @@ public class Win {
     public static boolean cell(int [][] board, int row){
         boolean rsl = true;
         for (int cell = 0; cell < board.length; cell++) {
-            if(board[row][cell] == 0){
+            if(board[row][cell] == 0) {
                 rsl = false;
                 break;
             }
